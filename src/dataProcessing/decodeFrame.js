3dba2f171c9969047bb02d7e13f4be3720052813
Charles_Bournonville <67782971+CarlosMagni@users.noreply.github.com>
2023-03-27 17:05:56 +0200

    feat: temperature and year in 16 bits, correct wrong offset (#5)

--- src/dataProcessing/decodeFrame.js
+++ src/dataProcessing/decodeFrame.js
@@ -5,20 +5,19 @@ const decodeFrame = (data) => {
     throw new Error("Data must be in binary format (Buffer)");
   }
   let parsedData = {};
-  let obj = { offset: 0 };
+  let obj = { offset: 1 };
   parsedData.numberOfHumiditySensors = readOneByte(data, obj);
   parsedData.humiditySensorsValue = [];
-  let staticOffset = obj.offset;
-  for (let i = obj.offset; i < parsedData.numberOfHumiditySensors + staticOffset; i++) {
+  for (let i = 0; i < parsedData.numberOfHumiditySensors; i++) {
     parsedData.humiditySensorsValue.push(readOneByte(data, obj));
   }
 
-  parsedData.waterTemperature = readOneByte(data, obj);
-  parsedData.waterLevel = readOneByte(data, obj);
+  parsedData.waterTemperature = readTwoBytes(data, obj)/10;
+  parsedData.waterLevel = readTwoBytes(data, obj);
   parsedData.phValue = readOneByte(data, obj);
   const day = readOneByte(data, obj);
   const month = readOneByte(data, obj)-1;
-  const year = 2000+readOneByte(data, obj);
+  const year = readTwoBytes(data, obj);
   const hour = readOneByte(data, obj);
   const minutes = readOneByte(data, obj);
 
@@ -31,6 +30,11 @@ const readOneByte = (data, obj) => {
   const val = data.readUInt8(obj.offset);
   obj.offset += 1;
   return val;
-};
+}
+const readTwoBytes = (data,obj) => {
+  const val = data.readUInt16BE(obj.offset);
+  obj.offset += 2;
+  return val;
+}
 
 module.exports = decodeFrame;
